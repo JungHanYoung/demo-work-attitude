9d45467ed01ff848f615c356bf87652b76a0ddbb
JeongHanYoung <8735132@gmail.com>
2019-04-30 15:43:09 +0900

    initialize project

---
+++ src/main/java/xyz/hanyoung/demoattachmentproject/DemoAttachmentProjectApplication.java
@@ -0,0 +1,13 @@
+package xyz.hanyoung.demoattachmentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAttachmentProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAttachmentProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/hanyoung/demoattachmentproject/DemoAttachmentProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.hanyoung.demoattachmentproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoAttachmentProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
